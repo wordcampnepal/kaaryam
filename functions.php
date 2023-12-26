42c58128cb4e2c0d056d90116d125cea02c71316
Racase Lawaju <codearryaas@gmail.com>
2023-12-26 15:19:35 +0545

    Change function names.

--- functions.php
+++ functions.php
@@ -1,56 +1,46 @@
 <?php
 /**
- * Starter FSE functions and definitions
+ * Kaaryam functions and definitions
  *
  * @link https://developer.wordpress.org/themes/basics/theme-functions/
  *
  * @package WordPress
- * @subpackage Starter FSE
- * @since Starter FSE 1.0
+ * @subpackage Kaaryam
+ * @since Kaaryam 1.0
  */
 
 
-if ( ! function_exists( 'starter_fse_support' ) ) :
+if ( ! function_exists( 'kaaryam_support' ) ) :
 
 	/**
 	 * Sets up theme defaults and registers support for various WordPress features.
 	 *
-	 * @since Starter FSE 1.0
+	 * @since Kaaryam 1.0
 	 *
 	 * @return void
 	 */
-	function starter_fse_support() {
-	
-		// Add support for block styles.
-		add_theme_support( 'wp-block-styles' );
-
+	function kaaryam_support() {
 		// Enqueue editor styles.
 		add_editor_style( 'style.css' );
-
-		// Experimental support for adding blocks inside nav menus
-		add_theme_support( 'block-nav-menus' );
-
-		// Add support for experimental link color control.
-		add_theme_support( 'experimental-link-color' );
 	}
 
 endif;
 
-add_action( 'after_setup_theme', 'starter_fse_support' );
+// add_action( 'after_setup_theme', 'kaaryam_support' );
 
-if ( ! function_exists( 'starter_fse_styles' ) ) :
+if ( ! function_exists( 'kaaryam_styles' ) ) :
 
 	/**
 	 * Enqueue styles.
 	 *
-	 * @since Starter FSE 1.0
+	 * @since Kaaryam 1.0
 	 *
 	 * @return void
 	 */
-	function starter_fse_styles() {
+	function kaaryam_styles() {
 		// Enqueue theme stylesheet.
 		wp_enqueue_style(
-			'starter_fse-style',
+			'kaaryam-style',
 			get_template_directory_uri() . '/style.css',
 			array(),
 			filemtime( get_theme_file_path( '/style.css' ) )
@@ -59,4 +49,4 @@ if ( ! function_exists( 'starter_fse_styles' ) ) :
 
 endif;
 
-add_action( 'wp_enqueue_scripts', 'starter_fse_styles' );
+add_action( 'wp_enqueue_scripts', 'kaaryam_styles' );
