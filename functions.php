1727ccbec63ff81edffa7064c3104045678d9ab1
Umesh Ghimire <me.umeshuser@gmail.com>
2023-12-31 13:47:17 +0545

    Fix - naming issue

--- functions.php
+++ functions.php
@@ -10,53 +10,55 @@
  */
 
 
-if ( ! function_exists( 'kaaryam_scripts' ) ) :
+if (!function_exists('kaaryam_theme_support')) :
 
-	/**
-	 * Sets up theme defaults and registers support for various WordPress features.
-	 *
-	 * @since Starter FSE 1.0
-	 *
-	 * @return void
-	 */
-	function kaaryam_scripts() {
-	
-		// Add support for block styles.
-		add_theme_support( 'wp-block-styles' );
+    /**
+     * Sets up theme defaults and registers support for various WordPress features.
+     *
+     * @return void
+     * @since Starter FSE 1.0
+     *
+     */
+    function kaaryam_theme_support()
+    {
 
-		// Enqueue editor styles.
-		add_editor_style( 'style.css' );
+        // Add support for block styles.
+        add_theme_support('wp-block-styles');
 
-		// Experimental support for adding blocks inside nav menus
-		add_theme_support( 'block-nav-menus' );
+        // Enqueue editor styles.
+        add_editor_style('style.css');
 
-		// Add support for experimental link color control.
-		add_theme_support( 'experimental-link-color' );
-	}
+        // Experimental support for adding blocks inside nav menus
+        add_theme_support('block-nav-menus');
 
-endif;
-
-add_action( 'after_setup_theme', 'kaaryam_scripts' );
-
-if ( ! function_exists( 'starter_fse_styles' ) ) :
-
-	/**
-	 * Enqueue styles.
-	 *
-	 * @since Starter FSE 1.0
-	 *
-	 * @return void
-	 */
-	function starter_fse_styles() {
-		// Enqueue theme stylesheet.
-		wp_enqueue_style(
-			'starter_fse-style',
-			get_template_directory_uri() . '/style.css',
-			array(),
-			filemtime( get_theme_file_path( '/style.css' ) )
-		);
-	}
+        // Add support for experimental link color control.
+        add_theme_support('experimental-link-color');
+    }
 
 endif;
 
-add_action( 'wp_enqueue_scripts', 'starter_fse_styles' );
+add_action('after_setup_theme', 'kaaryam_theme_support');
+
+if (!function_exists('kaaryam_scripts')) :
+
+    /**
+     * Enqueue styles.
+     *
+     * @return void
+     * @since Starter FSE 1.0
+     *
+     */
+    function kaaryam_scripts()
+    {
+        // Enqueue theme stylesheet.
+        wp_enqueue_style(
+            'kaaryam-style',
+            get_template_directory_uri() . '/style.css',
+            array(),
+            filemtime(get_theme_file_path('/style.css'))
+        );
+    }
+
+endif;
+
+add_action('wp_enqueue_scripts', 'kaaryam_scripts');
