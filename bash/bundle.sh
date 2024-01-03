040975490588205cebd3828cb8dbff0c2b16153e
Racase Lawaju <codearryaas@gmail.com>
2024-01-03 17:19:04 +0545

    Build and language script

---
+++ bash/bundle.sh
@@ -0,0 +1,20 @@
+bash ./bash/build.sh
+
+PACKAGE_NAME=$(cat package.json \
+  | grep name \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g' \
+  | tr -d '[[:space:]]')
+
+# Version key/value should be on his own line
+PACKAGE_VERSION=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g' \
+  | tr -d '[[:space:]]')
+
+cd bundle/$PACKAGE_VERSION
+
+zip -r ../$PACKAGE_NAME-$PACKAGE_VERSION.zip ./$PACKAGE_NAME/**
